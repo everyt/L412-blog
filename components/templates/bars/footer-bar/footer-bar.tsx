541141876ff6eb66388766dbaf65a64a84d85660
Everyhongcha <80094147+everyt@users.noreply.github.com>
2023-11-22 17:54:54 +0900

    Update footer-bar for use recoil

--- components/templates/bars/footer-bar/footer-bar.tsx
+++ components/templates/bars/footer-bar/footer-bar.tsx
@@ -2,30 +2,38 @@ import 'Styles/templates/footer-bar.scss';
 
 import Icons from './footer-icons';
 
+import { useRecoilValue } from 'recoil';
+import { getHueState } from 'States/getHueState';
+
 export default function FooterBar() {
+  const hue = useRecoilValue(getHueState);
+
   return (
     <footer className='footerbar-container-01'>
       <section className='footerbar-container-02'>
-        <figure className='footerbar-copyright'>
-          <span>©2023 Everyt.</span>
-          <span>All rights reserved.</span>
-        </figure>
         <Icons
           iconArray={[
             {
+              key: 1,
               icon: 'codicon:github',
               href: 'https://github.com/everyt',
             },
             {
+              key: 2,
               icon: 'basil:google-solid',
               href: 'https://mail.google.com/mail/?view=cm&fs=1&to=everytea.time@gmail.com',
             },
             {
+              key: 3,
               icon: 'ic:twotone-discord',
               href: 'https://discord.gg/BZ4Vs7fXz6',
             },
           ]}
         />
+        <div className='footerbar-copyright' style={{ color: `hsla(${hue},50%,50%,.35)` }}>
+          <span>©2023 Everyt.</span>
+          <span>All rights reserved.</span>
+        </div>
       </section>
     </footer>
   );
--- components/templates/bars/footer-bar/footer-icons.tsx
+++ components/templates/bars/footer-bar/footer-icons.tsx
@@ -1,30 +1,35 @@
 'use client';
 
-import 'Styles/templates/footer-bar.scss';
+import 'Styles/templates/footer-icons.scss';
 
 import { Icon } from '@iconify-icon/react';
 import { motion } from 'framer-motion';
-import { useNavigate } from 'react-router-dom';
+
+import { useRecoilValue } from 'recoil';
+import { getHueState } from 'States/getHueState';
 
 type FooterBarProps = {
   iconArray: iconArrayType[];
 };
 type iconArrayType = {
+  key: number;
   icon: string;
   href: string;
 };
 
 export default function Icons({ iconArray }: FooterBarProps) {
-  const navigate = useNavigate();
+  const hue = useRecoilValue(getHueState);
 
   const handleClickIcon = (event: React.MouseEvent, href: string) => {
     event.stopPropagation();
-    navigate(href);
+    window.open(href);
   };
 
   return iconArray.map((_) => (
-    <motion.figure
+    <motion.div
+      key={_.key}
       className='footerbar-icon'
+      style={{ color: `hsla(${hue},${40 + _.key * 5}%,${30 + _.key * 10}%,.${50 - _.key * 7})` }}
       whileHover={{
         scale: 1.2,
         transition: { duration: 0.3 },
@@ -34,6 +39,6 @@ export default function Icons({ iconArray }: FooterBarProps) {
       }}
       onClick={(event) => handleClickIcon(event, _.href)}>
       <Icon icon={_.icon} />
-    </motion.figure>
+    </motion.div>
   ));
 }
